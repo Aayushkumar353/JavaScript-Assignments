4907125f329287bf666bae18dc4300adc8d325c0
Aayushkumar353 <kumaraayush7877@gmail.com>
2026-03-15 15:27:43 +0530

    read from file

--- 01-async-js/easy/3-read-from-file.js
+++ 01-async-js/easy/3-read-from-file.js
@@ -5,3 +5,18 @@
 // Try to do an expensive operation below the file read and see how it affects the output. 
 // Make the expensive operation more and more expensive and see how it affects the output. 
 
+const fs = require("fs");
+const content = fs.readFile('a.txt', 'utf8', (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
+
+function heavyOperation() {
+  let sum = 0;
+  for (let i = 0; i < 5_000_000_000; i++) {
+    sum += i;
+  }
+  console.log("Heavy operation finished");
+}
+
+heavyOperation();
