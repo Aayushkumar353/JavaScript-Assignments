0d5d6b2ae3cefd934b983790bd79e6b78155b773
Aayushkumar353 <kumaraayush7877@gmail.com>
2026-03-15 14:38:04 +0530

    todo done

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,7 +12,31 @@
 */
 
 class Todo {
-
+  constructor(){
+    this.list = [];
+  }
+  add(str){
+    this.list.push(str);
+  }
+  remove(index){
+    if(index>= this.list.length || index <0) return;
+    this.list.splice(index,1);
+  }
+  update(index, updatedTodo){
+    if(index>= this.list.length || index <0) return;
+    
+    this.list[index] = updatedTodo;
+  }
+  getAll(){
+    return this.list;
+  }
+  get(index){
+    if(index>= this.list.length || index <0) return null;
+    return this.list[index];
+  }
+  clear(){
+    this.list = [];
+  }
 }
 
 module.exports = Todo;
