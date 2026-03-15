3dc73a678b8255b40a8553fe2ac370d7f634c9c9
Aayushkumar353 <kumaraayush7877@gmail.com>
2026-03-15 17:53:44 +0530

    click

--- 01-async-js/medium/2-clock.js
+++ 01-async-js/medium/2-clock.js
@@ -6,3 +6,19 @@
 //  - HH:MM::SS (Eg. 13:45:23)
 
 //  - HH:MM::SS AM/PM (Eg 01:45:23 PM)
+
+const counter = () => {
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let ampm = hours >= 12 ? "PM" : "AM";
+
+    hours = hours%12;
+    hours = hours === 0 ? 12 : hours;
+
+    console.log(`${hours}:${minutes}:${seconds} ${ampm}`)
+    setTimeout(counter,1000);
+}   
+
+setTimeout(counter,1000);
