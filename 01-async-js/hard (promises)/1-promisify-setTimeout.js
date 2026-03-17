539a552bffbd72e369957c7414448db120cbf803
Aayushkumar353 <kumaraayush7877@gmail.com>
2026-03-17 12:44:04 +0530

    pomise set timeout

--- 01-async-js/hard (promises)/1-promisify-setTimeout.js
+++ 01-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -3,12 +3,12 @@
 */
 
 function wait(n) {
-    let p = new Promise((resolve) => {
-      setTimeout(() => {
-        resolve();
-      }, n * 1000);
-    });
-    return p;
-  }
+  let p = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+      resolve()
+    },n*1000)
+  })
+  return p;
+}
   
   module.exports = wait;
